b8bec2ac7469f8cb2c03616f6aa7d6760012d8fa
StroschneiderFabian <stro.fabian@gmail.com>
2023-06-05 19:49:11 +0200

    database created - see TODO

--- data.ts
+++ data.ts
@@ -1,5 +1,5 @@
-import { Database as Driver} from "sqlite3";
-import { open, Database } from "sqlite";
+import {Database as Driver} from "sqlite3";
+import {open, Database} from "sqlite";
 
 export const dbFileName = 'internship.db';
 
@@ -10,6 +10,8 @@ export class DB {
             driver: Driver
         });
         await this.ensureTablesCreated(db);
+        await DB.ensureSampleDataInserted(db);
+
         return db;
     }
 
@@ -25,7 +27,65 @@ export class DB {
         await connection.run('rollback;');
     }
 
+    public static async ensureSampleDataInserted(connection: Database): Promise<void> {
 
+        await connection.run(`DELETE
+                              FROM Firma;`);
+
+        await connection.run(`INSERT INTO Firma
+                              VALUES (1, 'HTL Leonding', 'htl@leonding', '+43 54561561');`);
+
+
+        await connection.run(`DELETE
+                              FROM Praktikum;`);
+
+        await connection.run(`INSERT INTO Praktikum
+                              VALUES (1, 'Praktikant', 'irgendwas', 30, 'kompetent', '05-07-2023');`);
+
+
+        await connection.run(`DELETE
+                              FROM Schueler;`);
+
+        await connection.run(`INSERT INTO Schueler
+                              VALUES (1, 'Fabian Stroschneider', 'f.stro@htl', '4020 Linz', '+43 7867676876786');`);
+    }
+
+    // TODO: Add praktikum (see cld)
     private static async ensureTablesCreated(connection: Database): Promise<void> {
+        await connection.run(`CREATE TABLE IF NOT EXISTS Firma
+                              (
+                                  id        INTEGER   NOT NULL,
+                                  name      TEXT      NOT NULL,
+                                  email     TEXT      NOT NULL,
+                                  telefon   TEXT      NOT NULL,
+--                                   praktikum Praktikum NULL,
+                                  CONSTRAINT PK_Firma primary key (id)
+                              ) strict`);
+        // TODO: Add Firma, Schueler (see cld)
+
+        await connection.run(`CREATE TABLE IF NOT EXISTS Praktikum
+                              (
+                                  id              INTEGER  NOT NULL,
+                                  titel           TEXT     NOT NULL,
+                                  beschreibung    TEXT     NOT NULL,
+                                  dauertage       INTEGER  NOT NULL,
+                                  anforderungen   TEXT     NOT NULL,
+--                                   firma           Firma    NOT NULL,
+--                                   schueler        Schueler NOT NULL,
+                                  anmeldungsdatum TEXT     NOT NULL,
+                                  CONSTRAINT PK_Praktikum primary key (id)
+                              ) strict`);
+
+        // TODO: Add Praktikum (see cld)
+        await connection.run(`CREATE TABLE IF NOT EXISTS Schueler
+                              (
+                                  id        INTEGER   NOT NULL,
+                                  name      TEXT      NOT NULL,
+                                  email     TEXT      NOT NULL,
+                                  adresse   TEXT      NOT NULL,
+                                  telefon   TEXT      NOT NULL,
+--                                   praktikum Praktikum NOT NULL,
+                                  CONSTRAINT PK_Schueler primary key (id)
+                              ) strict`);
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,30 @@
-//import {DB, ensureSampLeDataInserted} from './data';
+import { DB } from "../data";
 // @ts-ignore
-import express from 'express';
+import express, {Express, request, response} from "express";
 // @ts-ignore
 import cors from "cors";
 
-const app = express();
-app.use(cors());
+import firmenRouter from "./routers/firmen-router";
+import praktikaRouter from "./routers/praktikum-router";
+import schuelerRouter from "./routers/schueler-router";
+
+const port: number = 3000;
+const app: Express = express();
+
 app.use(express.json());
+app.use(cors());
 
+app.use('/api/firmen', firmenRouter);
+app.use('/api/praktika', praktikaRouter);
+app.use('/api/schueler', schuelerRouter);
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+(async () => {
+    const db = await DB.createDBConnection();
+    // await db.run('drop table Schuler;');
+    // await db.run('drop table Firma;');
+    // await db.run('drop table Praktikum;');
+})();
+
+app.listen(port, (): void => {
+    console.log(`[Server]  server is now running at http://localhost:${port}`);
 });
-
